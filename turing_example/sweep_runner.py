9f17abe4063abcde5921650ef1caa3aeae9912de
Randy Paffenroth <randy.paffenroth@gmail.com>
2026-04-06 10:50:28 -0400

    Turing stuff

---
+++ turing_example/sweep_runner.py
@@ -0,0 +1,29 @@
+# Import the W&B Python Library and log into W&B
+import wandb
+import click
+import random
+import math
+
+# Define objective/training function
+def objective(config,i):
+    score = math.sqrt(config['coolparameter1']**3/(i+1)) + config['coolparameter2'] + random.uniform(0,0.5)
+    return score
+
+# Define the command line options
+@click.command()
+@click.option("--coolparameter1", default=1.0)
+@click.option("--coolparameter2", default=1.0)
+@click.option("--lr", default=0.01)
+@click.option("--modeltype", default='mymodel-type')
+@click.option("--epochs", default=100)
+
+# The main loop
+def main(**kw):
+    wandb.init(project="dscs533-turing-example", config=kw)
+    for i in range(kw['epochs']): 
+        score = objective(kw,i)
+        wandb.log({"score": score})
+
+if __name__=="__main__":
+    main()    
+1
